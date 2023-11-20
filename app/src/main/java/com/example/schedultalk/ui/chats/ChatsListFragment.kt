405c08b5bf6d442c4fc1808d437dc91029ad065e
HeesuAhn0 <heesuya617@naver.com>
2023-11-21 07:17:40 +0900

    chat update

--- app/src/main/java/com/example/schedultalk/ui/chats/ChatsListFragment.kt
+++ app/src/main/java/com/example/schedultalk/ui/chats/ChatsListFragment.kt
@@ -1,40 +1,41 @@
 package com.example.schedultalk.ui.chats
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.schedultalk.databinding.FragmentChatsListBinding
+import com.example.schedultalk.ui.schedule.ScheduleFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatsListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatsListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    val chats = arrayOf(
+        Chat(EUser.Heesu, "안희수", "안녕하세요!", "4분 전"),
+        Chat(EUser.Seunghwan, "안승환", "뭐해?", "11월 6일"),
+        Chat(EUser.Haejin, "정해진", "회의 어디서 할까요?", "11월 6일"),
+    )
+    lateinit var binding: FragmentChatsListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val binding = FragmentChatsListBinding.inflate(inflater, container, false)
-        return binding.root
+        this.binding = FragmentChatsListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // RecyclerView ID에 'binding' 객체 사용
+        this.binding.recChats.layoutManager = LinearLayoutManager(requireContext())
+        this.binding.recChats.adapter = ChatsAdapter(chats)
+
+        return view
     }
 
     companion object {
         @JvmStatic
         fun newInstance(me: String) =
-            ChatsListFragment().apply {
+            ScheduleFragment().apply {
                 arguments = Bundle().apply {
                     putString("me", me)
                 }
